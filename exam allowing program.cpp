d52eb7ddc53e466559c59efc01c1a410d3cc24cd
faisalrehman-tech <durranifaisal344@gmail.com>
2025-01-23 11:43:01 +0500

    Add files via upload

---
+++ exam allowing program.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   line
+#include <iostream>
+using namesoace std;
+int main() {
+    int attendance;
+    cout<<"enter your attendance";
+    cin>>attendance;
+    if
+    (age<=75)
+    {
+        cout<<"Allow for exam ";
+    }
+    else{
+        cout<<"Not allow for Exam:"<<endl;
+    }
+    return 0;
+}
+    return 0;
+}
